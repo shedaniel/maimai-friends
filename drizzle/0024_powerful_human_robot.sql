6198a566a5a4b232c26563f8a2530cfa9550742d
shedaniel <daniel@shedaniel.me>
2025-11-02 01:20:29 +0800

    remove added migration

--- drizzle/0024_powerful_human_robot.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX `songs_region_b50_idx`;--> statement-breakpoint
-ALTER TABLE `songs` DROP COLUMN `b50`;--> statement-breakpoint
-DROP INDEX `user_scores_snapshotid_idx`;--> statement-breakpoint
-ALTER TABLE `user_scores` ADD `rank` integer;--> statement-breakpoint
-CREATE INDEX `user_scores_snapshotid_rank_idx` ON `user_scores` (`snapshotId`,`rank`);
